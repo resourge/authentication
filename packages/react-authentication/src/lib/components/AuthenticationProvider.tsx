6232e19d87b65051fde3f99a1f6f6036201bb913
José Silva Oliveira <jose.silva.oliveira@devoteam.com>
2023-10-27 17:37:21 +0100

    fix(authenticationprovider): fix not saving the current token in localStorage

--- packages/react-authentication/src/lib/components/AuthenticationProvider.tsx
+++ packages/react-authentication/src/lib/components/AuthenticationProvider.tsx
@@ -117,13 +117,14 @@ function AuthenticationProvider<
 		const loginAuth = await onLogin!(userNameOrEmail, password);
 
 		if ( loginAuth && loginAuth.token !== null ) {
-			const auth = await authentication.promise(loginAuth.token);
+			const { token, refreshToken } = loginAuth
+			const auth = await authentication.promise(token);
 			const user = auth.user
 			const permissions = auth.permissions
 
 			setAuthentication({
 				token,
-				refreshToken: loginAuth.refreshToken,
+				refreshToken,
 				user: user ?? new BaseUser() as U,
 				permissions: permissions ?? new BasePermissions() as P
 			})
