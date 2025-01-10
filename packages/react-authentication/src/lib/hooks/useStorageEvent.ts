8b8f420901bd4707bfe8ee40b95430477b354421
resourge <jdaoliveiraprograming@hotmail.com>
2025-01-10 14:50:59 +0000

    fix(usestorageevent): prevent multiple onStorageChanges from accuring

--- packages/react-authentication/src/lib/hooks/useStorageEvent.ts
+++ packages/react-authentication/src/lib/hooks/useStorageEvent.ts
@@ -2,15 +2,25 @@ import { useEffect } from 'react';
 
 import { StorageKeys } from '../utils/constants';
 
+function debounce(func: () => void, wait: number) {
+	let timer: NodeJS.Timeout;
+	return function() {
+		clearTimeout(timer);
+		timer = setTimeout(func, wait);
+	};
+}
+
 export const useStorageEvent = globalThis.window && typeof globalThis.window.addEventListener !== 'undefined' 
 	? (onStorageChange: () => void) => {
 		useEffect(() => {
+			const _onStorageChange = debounce(onStorageChange, 400);
 			const onStorage = (e: StorageEvent) => {
 				const {
 					key, newValue, oldValue 
 				} = e;
-				if (key && oldValue && StorageKeys.includes(key) && newValue !== oldValue) {
-					onStorageChange();
+
+				if (key && StorageKeys.includes(key) && newValue !== oldValue) {
+					_onStorageChange();
 				}
 			};
 			window.addEventListener('storage', onStorage, true);
